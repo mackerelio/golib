31b662af3677baa0dd894514162ebb2169988f57
Songmu <y.songmu@gmail.com>
2016-02-24 19:42:48 +0900

    fix scale

--- uptime/uptime_windows.go
+++ uptime/uptime_windows.go
@@ -6,5 +6,5 @@ var getTickCount = syscall.NewLazyDLL("kernel32.dll").NewProc("GetTickCount")
 
 func get() (float64, error) {
 	r, _, err := getTickCount.Call()
-	return float64(r), err
+	return float64(r) / 1000, err
 }
