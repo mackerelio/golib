d95a7bc89a18745cc2d8d686938296e5369d8eef
itchyny <itchyny@hatena.ne.jp>
2017-10-16 17:20:14 +0900

    uptime should be positive

--- uptime/uptime_test.go
+++ uptime/uptime_test.go
@@ -9,5 +9,8 @@ func TestGet(t *testing.T) {
 	if err != nil {
 		t.Errorf("error should be nil but: %s", err)
 	}
+	if v <= 0.0 {
+		t.Errorf("uptime should be positive but got: %f", v)
+	}
 	t.Logf("uptime: %f", v)
 }
